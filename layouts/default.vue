0fd4d43514764d57b08bb7d1054d68e703ad73f3
Maksim Leali <maksimleali@Maksims-Mac-mini.local>
2022-10-24 11:47:05 +0200

    firsts try threejs + vue

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template lang="pug"> 
+  .main
+    .stage.fixed.outline-none
+    Nuxt.relative.z-10
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style >
+.stage{
+    outline: 0;
+    box-sizing: border-box;
+    border-width: 0;
+    border-style: solid;
+    border-color: #e2e8f0;
+    position: fixed;
+        overflow: hidden;
+    margin: 0px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+.content.p-5
+  button(v-for="(obj, index) in objects" :key="index" @click="$stage.lookAt({ index, zoom:obj.zoom })" ) {{ obj.name }}
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      objects: [
+        {
+          name: "Cube",
+          zoom: 1.5
+        },
+        {
+          name: "Ico"
+        },
+        {
+          name: "Sphere",
+          zoom: 1.8
+        }
+      ]
+    };
+  },
+  mounted() {
+    console.log(this.$stage);
+  }
+};
+</script>
